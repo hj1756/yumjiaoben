77920227cd27be507cd7f0e07233866ea3c263b2
hsh <root@localhost.localdomain>
2018-10-11 15:05:01 +0800

    新添加的yumjiaoben

---
+++ xuni-myyum.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ ! -d "/yum" ] ; then
+mkdir /yum
+else
+    echo "yum已经存在！"
+fi
+
+
+if [ ! -d "/etc/yum.repos.d/back" ] ; then
+    mkdir /etc/yum.repos.d/back
+else
+    echo "back已经存在！"
+fi
+
+mv /etc/yum.repos.d/*.repo  /etc/yum.repos.d/back
+
+touch /etc/yum.repos.d/myyum.repo
+chmod +x /etc/yum.repos.d/myyum.repo
+
+echo -e "[Centos]\nname=Centos7\nbaseurl=file:///yum\ngpgcheck=0\nenabled=1"\
+        > /etc/yum.repos.d/myyum.repo
+
+mount /dev/sr0 /yum
+
+yum repolist
